a1b8be99c41233e6f6fdc855b8fbab42337cb4c9
aresdevv <giovaniarias06@gmail.com>
2025-06-04 21:28:12 -0500

    login, tipografia, color base

--- src/components/Login/InputLogin.jsx
+++ src/components/Login/InputLogin.jsx
@@ -1,10 +1,11 @@
-export default function InputLogin() {
+export default function InputLogin({ name, placeholder, value, type }) {
   return (
     <input
-      className="border-2 border-white rounded-lg"
-      type="text"
-      name=""
-      value=""
+      className="border-2 border-white rounded-lg w-full placeholder-gray-400 px-2 font-bold h-[36px]"
+      type={type}
+      name={name}
+      value={value}
+      placeholder={placeholder}
     />
   );
 }
--- src/sections/Login/FormLogin.jsx
+++ src/sections/Login/FormLogin.jsx
@@ -5,12 +5,14 @@ export default function FormLogin() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <div className="border-solid border-2 border-white rounded-full p-4 mb-6">
-        <UserIcon className={"size-28"} />
+        <UserIcon className={"size-28 text-white"} />
       </div>
-      <form className="flex gap-4 flex-col justify-center items-center p-6 rounded shadow-md w-96">
-        <InputLogin />
-        <InputLogin />
-        <button type="">kalla kchudo</button>
+      <form className="flex gap-4 flex-col justify-center items-center p-6 rounded  w-72">
+        <InputLogin placeholder={"Username"} type={"text"} />
+        <InputLogin placeholder={"Password"} type={"password"} />
+        <button className="bg-white w-full rounded-lg h-10 font-bold" type="">
+          Iniciar Sesión
+        </button>
       </form>
     </div>
   );
